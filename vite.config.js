094518008e92e2f8a708ae490521447a75e90152
Adhitvora <voraadhit2096@gmail.com>
2026-06-15 11:37:25 +0530

    refactor: transition manualChunks to function-based grouping in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,19 +4,35 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/franchise/",
   plugins: [react()],
-  build: {
-    target: "es2020",
-    cssCodeSplit: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          motion: ["framer-motion"],
-          forms: ["react-hook-form", "zod", "@hookform/resolvers"],
-          vendor: ["react", "react-dom", "axios"],
-        },
+ build: {
+  target: "es2020",
+  cssCodeSplit: true,
+  rollupOptions: {
+    output: {
+      manualChunks(id) {
+        if (id.includes("framer-motion")) {
+          return "motion";
+        }
+
+        if (
+          id.includes("react-hook-form") ||
+          id.includes("zod") ||
+          id.includes("@hookform/resolvers")
+        ) {
+          return "forms";
+        }
+
+        if (
+          id.includes("react") ||
+          id.includes("react-dom") ||
+          id.includes("axios")
+        ) {
+          return "vendor";
+        }
       },
     },
   },
+},
   test: {
     environment: "jsdom",
     globals: true,
